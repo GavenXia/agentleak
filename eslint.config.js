fc30f17aede7d90fcb3817aa6b9b65f663a22951
xiaminghua <xiaminghua@linklogis.com>
2026-09-12 20:35:12 +0800

    chore: scaffold typescript project with eslint, prettier, husky and commitlint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist/', 'node_modules/', 'coverage/', '*.cjs'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+  prettier,
+);
